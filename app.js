48f32e7f417153c09282ffd8b79f7a62db50f6f1
Reuben Dickson <reubendicksonebong@gmail.com>
2025-07-11 07:34:45 -0400

    Implement basic testing with Mocha

--- app.js
+++ app.js
@@ -6,28 +6,27 @@ import authRoutes from './routes/authRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import questionRoutes from './routes/questionRoutes.js';
 import examRoutes from './routes/examRoutes.js';
-// import studentExamRoutes from './routes/studentExamRoutes';
+import studentExamRoutes from './routes/studentExamRoutes.js';
 
 dotenv.config();
 
 const app = express();
-
 connectDB();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes); // Mount authentication routes
-app.use("/api/admin/auth", adminRoutes); // Mount admin management routes
-// This code initializes an Express.js application for a Computer-Based Test (CBT) system backend.
 
-app.use("/api/questions", questionRoutes); // Mount question management routes
-app.use("/api/exams", examRoutes); // Mount exam management routes
+// TODO: Mount API routes here (students, courses, questions, etc)
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/questions', questionRoutes);
+app.use('/api/exams', examRoutes);
+app.use('/api/student-exams', studentExamRoutes);
+// Handle 404 errors
 
 // Routes
 app.get('/', (req, res) => {
     res.send('🎓 CBT System Backend is running...');
 });
 
-// TODO: Mount API routes here (students, courses, questions, etc)
 export default app;
--- server.js
+++ server.js
@@ -1,15 +1,4 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/db.js';
-import cors from 'cors';
-import authRoutes from './routes/authRoutes.js';
-import adminRoutes from './routes/adminRoutes.js';
-import questionRoutes from './routes/questionRoutes.js';
-import examRoutes from './routes/examRoutes.js';
-import studentExamRoutes from './routes/studentExamRoutes.js';
-
-
-// TODO: Mount API routes here (students, courses, questions, etc)
+import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
