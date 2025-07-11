f42769edd25d8e182f4ec182ac6a60c3580497e2
Reuben Dickson <76810397+ReubenDickson@users.noreply.github.com>
2025-07-11 07:18:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,23 @@ import questionRoutes from './routes/questionRoutes.js';
 import examRoutes from './routes/examRoutes.js';
 import studentExamRoutes from './routes/studentExamRoutes.js';
 
-
 // TODO: Mount API routes here (students, courses, questions, etc)
+const app = express();
+dotenv.config();
+connectDB();
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api/questions', questionRoutes);
+app.use('/api/exams', examRoutes);
+app.use('/api/student-exams', studentExamRoutes);
+// Handle 404 errors
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('🎓 CBT System Backend is running...');
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
